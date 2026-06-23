e6a90d5038651ad5634509e8ba580be2a8cd0192
1210walnut <naookamaine1210@gmail.com>
2026-06-23 18:30:37 +0900

    script.js を作成

---
+++ Youtube/script.js
@@ -0,0 +1,43 @@
+const API_KEY = "ここにAPIキー";
+
+async function search() {
+
+    const q = document.getElementById("query").value;
+
+    const url =
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=20&q=${encodeURIComponent(q)}&key=${API_KEY}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const results = document.getElementById("results");
+    results.innerHTML = "";
+
+    data.items.forEach(video => {
+
+        const div = document.createElement("div");
+        div.className = "video";
+
+        div.innerHTML = `
+            <img src="${video.snippet.thumbnails.medium.url}">
+            <p>${video.snippet.title}</p>
+        `;
+
+        div.onclick = () => {
+            play(video.id.videoId);
+        };
+
+        results.appendChild(div);
+    });
+}
+
+function play(videoId){
+
+    document.getElementById("player").innerHTML = `
+        <iframe
+            src="https://www.youtube.com/embed/${videoId}?autoplay=1"
+            frameborder="0"
+            allowfullscreen>
+        </iframe>
+    `;
+}
